b99efe8ba7b6d826df3e05ca7a869378cc2f061f
Lucas Benedetti <lucas.benedetti6@gmail.com>
2022-09-01 20:59:34 -0300

    Quite variables innecesarias y la mayoria de console.log()

--- js/almacenar.js
+++ js/almacenar.js
@@ -7,41 +7,35 @@ document.addEventListener('DOMContentLoaded', () => {
         let contenedor = document.getElementById('contenedor');
         contenedor.innerHTML = '';
         if (localStorage.getItem('lista')){
-        cargaWeb = JSON.parse(localStorage.getItem('lista'));
-        console.log('cargando');
+            cargaWeb = JSON.parse(localStorage.getItem('lista'));
+            console.log('cargando');
             for (item of cargaWeb){
-            test.push(item)
-            contenedor.innerHTML += item;
-        }
+                test.push(item)
+                contenedor.innerHTML += item;
+            }
         
-    }
+        }
         document.getElementById('agregar').addEventListener('click', () =>{
-        let  item = document.getElementById('item').value;
-        if (item.trim().length !== 0){
-        items.push(`<li class='list-group-item'>${item}</li>`);      
-        console.log(items);
-        let agregarContenedor = (items);
-        console.log('for loop');
-        console.log('AGREGAR CONTENEDOR',agregarContenedor);
-        
-        for(let i = 0; i < agregarContenedor.length; i++){
-            let valor = agregarContenedor[i];
-            test.push(valor);
-            contenedor.innerHTML += agregarContenedor[i];
-            agregarContenedor.splice(i,1);
-        }
-        localStorage.setItem('lista', JSON.stringify(test));
-        console.log(items);
-        console.log(test,'20');
-    };
-});
+            let  item = document.getElementById('item').value;
+            if (item.trim().length !== 0){
+                items.push(`<li class='list-group-item'>${item}</li>`);     
+                for(let i = 0; i < items.length; i++){
+                    let valor = items[i];
+                    test.push(valor);
+                    contenedor.innerHTML += items[i];
+                    items.splice(i,1);
+                }
+                localStorage.setItem('lista', JSON.stringify(test));
+                console.log(items);
+                console.log(test,'20');
+            };
+        });
         document.getElementById('limpiar').addEventListener('click', () =>{
-            //Borrando
+        //Borrando
             console.log('Borrando')
-        agregarContenedor = '';
-        items=[];
-        test = [];
+            items=[];
+            test = [];
             localStorage.removeItem('lista');
-           contenedor.innerHTML = '';
+            contenedor.innerHTML = '';
         });
     });
